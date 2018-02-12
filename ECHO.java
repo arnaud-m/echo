1d3ab4c413ea2cc3895940c5afa7e61bfc21f974
Arnaud Malapert <arnaud.malapert@unice.fr>
2018-02-12 17:17:00 +0100

    comment java and rkt answers close #1

--- ECHO.java
+++ ECHO.java
@@ -3,8 +3,12 @@ import java.util.Scanner;
 public class ECHO {
  
   public static void main(String[] args) {
+      // Lire un vecteur d'entiers depuis l'entrée standard.
+      // La classe scanner permet de lire facilement des types primitifs et des chaînes de caractères.
       final Scanner sc = new Scanner(System.in);
+      // Tant qu'il reste un entier à lire
       while(sc.hasNextInt()) {
+          // on écrit directement l'entier sur la sortie standard (sans le stocker)
           System.out.println(sc.nextInt());
       }
   }
--- ECHO.rkt
+++ ECHO.rkt
@@ -1,9 +1,37 @@
 #lang racket
 
-(define (echo)
-  (define n (read))
-  (when (not (eof-object? n))
-    (printf "~a~n" n)
-    (echo)))
+; Lit jusqu'à n données sur l'entrée standard. Si le paramètre n est omis, la
+; lecture se poursuit jusqu'au caractère fin de fichier.
+;
+; Pour rappel, une donnée est n'importe quelle valeur valide en Racket : entier,
+; caractère, chaînes de caractères, listes, symbole... Le format doit respecter
+; la norme du langage (cf fonctions read et write). 
+(define (read-data [n +inf.0])
+  ; test pour savoir si on doit lire une donnée
+  (if (> n 0) 
+      ; lecture de la donnée
+      (let ([datum (read)])
+        ; vérification de la validité de la donnée
+        (if (eof-object? datum) 
+            ; la fin de fichier est atteinte, aucune donnée disponible
+            ; la liste vide est renvoyée
+            empty
+            ; ajout de la donnée au résultat de l'appel récursif
+            (cons datum (read-data (- n 1)))))
+      ; pas de donnée à lire, la liste vide est renvoyée
+      empty))
 
+; Affiche ligne par ligne sur la sortie standard une liste de données Racket.
+(define (print-data L)
+  ; Pour chaque donnée de la liste
+  (for ([datum (in-list L)])
+    ; Afficher la donnée et sauter une ligne
+    (printf "~s~n" datum)))
+
+; Lit les données présentes sur l'entrée standard et les affiches ligne par
+; ligne sur la sortie standard
+(define (echo)
+  (print-data (read-data)))
+
+; Fin des définitions, point d'entrée du programme.
 (echo)

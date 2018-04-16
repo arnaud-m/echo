4db871eee8960713abaa4f588b992d21a7f4e3a6
Arnaud Malapert <arnaud.malapert@unice.fr>
2018-04-16 15:28:18 +0200

    fix ECHO.py (python 2)

--- ECHO.py
+++ ECHO.py
@@ -6,10 +6,12 @@ import sys
 ## Stockage des données dans une liste.
 ## Suppression des caractères d'espacement au début et à la fin de chaque chaîne avec strip.
 x = [s.strip() for s in sys.stdin.readlines()]
+## Suppression des chaines vides
+x = filter( lambda s : len(s) > 0, x)
 
 ## Traitement des données 
 ## Ici, il n'y a rien à faire pour echo.
 y = x
 
 ## Écriture du résultat sur la sortie standard
-sys.stdout.write("\n".join(n))
+sys.stdout.write("\n".join(y))

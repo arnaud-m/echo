e59279b774f1be13631cfe994e88edf571a1396a
Arnaud Malapert <arnaud.malapert@unice.fr>
2019-03-08 17:41:40 +0100

    add modern c++ answer close #7

--- ECHO.cpp
+++ ECHO.cpp
@@ -1,25 +1,27 @@
 #include <iostream>
 #include <string>
-#include <cctype>
-#include <algorithm>
+#include <sstream> 
 
-//Nous avons besoin de pouvoir manipuler des entrées/sorties et des string
+using namespace std;
 
 int main() {
+    // Vous pouvez remplacer le int par n'importe quel type primitif et le reste de la lecture ne change pas.
     int i;
-    // On boucle sur l'entrée standard. On place chaque message dans la
-    // string s, puis on le traite
-    for (std::string s; getline(std::cin, s);) {
-        // On supprime les caractères blancs.
-        // Se référer à la doc. C++ pour string::erase et std::remove_if
-        s.erase(std::remove_if(s.begin(), s.end(), ::isspace), s.end());
-        // Ici un traitement : (pas utile pour echo)
+    
+    // /!\ On suppose que le fichier est bien formé : on ne gère pas les erreurs.
 
-        // Affichage (si la chaîne n'est pas vide), on affiche l'entier correspondant à la chaîne
-        // suivi d'une nouvelle ligne :
-        if(s.size()){
-            i = stoi(s, nullptr, 10);
-            std::cout << i << std::endl;
+    // On boucle sur les lignes de l'entrée standard.
+    // On place chaque message dans la string s, puis on le traite
+    for (string s; getline(cin, s);) {
+        // on lit les entiers présents sur la ligne courante.
+        // Ici, il n'y a qu'un entier qu'on affiche sur une ligne.
+        istringstream stream(s);
+        if(s.size() >0) {
+          // La ligne n'est pas vide
+          do {
+            stream >> i;
+            cout << i << endl;
+          } while (stream.good());
         }
     }
     return 0;
